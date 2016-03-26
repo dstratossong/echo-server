0e6653723ec6ae8b2d2e4aab357744639aa15a04
David Song <david.song0802@gmail.com>
2016-03-26 14:12:26 -0400

    added client side; refactored backend output

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # echo-server
 A simple echo server with a plain front end. To help with building web hooks.
+
+
+### Usage
+
+```
+npm install
+node server.js 4009 4010
+```
--- server.js
+++ server.js
@@ -1,28 +1,78 @@
-var http = require('http');
-// var url = require('url');
+"use strict";
 
-var port = process.argv[2];
+var http = require('http'),
+    url = require('url');
+var WebSocketServer = require('ws').Server;
+var Router = require('node-simple-router');
 
-function writeResponse(response) {
-	response.writeHead(200, "OK", {'Content-Type': 'text/html'});
-	response.end("");
+var echoPort = process.argv[2],
+    httpPort = process.argv[3],
+    socketPort = 4011;
+
+// --- socket ---
+var wss = new WebSocketServer({ port: socketPort });
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
+
+// --- http ---
+var router = new Router({static_route: __dirname + '/static'});
+
+http.createServer(router)
+    .listen(httpPort);
+
+// --- echo ---
+http.createServer(function (request, response) {
+	writeMeta(request);
+
+  if (request.method == 'POST') {
+    writeBody(request, response);
+  } else {
+		writeParams(request, response);
+    writeResponse(response);
+  }
+}).listen(echoPort);
+
+function writeMeta(request) {
+	var date = new Date(),
+	    outStr = "\n --- New " + request.method + " Request --- \n" +
+	             "date: " + date.toLocaleDateString() + " - " + date.toLocaleTimeString() + "\n" +
+               "header: \n" +
+	             JSON.stringify(request.headers, null, 2);
+
+	wss.broadcast(outStr);
+	console.log(outStr);
 }
 
-var server = http.createServer(function (request, response) {
-	console.log("Request: " + request.method);
-	console.log(request.headers);
+function writeBody(request, response) {
+	var outStr = "body: \n";
 
-	if (request.method == 'POST') {
-		request.on('data', function(chunk) {
-			console.log("Received body data:");
-			console.log(chunk.toString());
-		});
-		request.on('end', function() {
-			writeResponse(response);
-		});
-	} else {
+	request.on('data', function(chunk) {
+		outStr += chunk.toString() + "\n";
+	});
+	request.on('end', function() {
+		wss.broadcast(outStr);
+		console.log(outStr);
+
 		writeResponse(response);
-	}
-});
+	});
+}
 
-server.listen(port);
+function writeParams(request, response) {
+	var urlParts = url.parse(request.url, true),
+	    outStr = "params: \n" +
+			         JSON.stringify(urlParts.query, null, 2);
+
+	wss.broadcast(outStr);
+	console.log(outStr);
+
+	writeResponse(response);
+}
+
+function writeResponse(response) {
+  response.writeHead(200, "OK", { 'Content-Type': 'text/html' });
+  response.end("");
+}
